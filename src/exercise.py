1d7f5e20a6c79218767fd6b98a9982791ada287b
MrSullivan <>
2021-04-20 19:36:07 +0000

    while true, number variable with interger and user input, if statement, comparison, print and continue, elif, compare, print and multiply number, continue, else and then break.

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,18 @@
 def main():
     #write your code below this line
+    while True:
+      number = int(input("Give a number:"))
+
+      if (number<0):
+        print("Unsuitable number")
+        continue
+      elif (number>0):
+        print(number * number)
+        continue
+      else:
+        break
+
+
 
 if __name__ == '__main__':
     main()
